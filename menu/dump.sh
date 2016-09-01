0d465cb948e8d85ebd22ab6273e5177232de34dd
siquel <squual@kahvipaussi.net>
2016-09-01 21:27:19 +0300

    Add alternate route if there is desert

--- menu/dump.sh
+++ menu/dump.sh
@@ -8,27 +8,32 @@ if [[ -z "$url" ]]; then
     url=$(curl -s http://www.kajaani.fi/fi/mamselli/opiskelijaravintoloiden-ruokalistat | grep -P "fox_vko_[0-9]{1,2}_linjasto_1_ja_2" | sed -r 's/.+?\"(http.+?pdf)\".+/\1/')
 fi
 
-curl "$url" -s -o /tmp/ruokaa.pdf
+curl "$url" -o /tmp/ruokaa.pdf
 
 # dump pdf to txt
 file=$(pdftotext /tmp/ruokaa.pdf -)
+
+
+if [[ $file == *"lkiruoka"* ]]; then
+    echo "jälkkäri"
+else
 # cut 44 first lines (wednesday start on #45) 
 # cut to EOF at "Keskiviikko"
 cutted=$(echo "$file" | sed -e '1,44d' | sed -n '/Keskiviikko/q;p')
 
 # offset where to cut
-x=12
+#x=12
 # n is chosen to start as 2 because we save the start line
 # cut 6 times
-for n in {2..7}; do
-    cutted=$(echo "$cutted" | sed -e "$n,$x d"); 
+#for n in {2..7}; do
+ #   cutted=$(echo "$cutted" | sed -e "$n,$x d"); 
     # increment offset
-    x=$((x+1));
-done
+  #  x=$((x+1));
+#done
 
 # cut Tiistai and \n
 # move 4 bottom lines to top (monday and tuesday)
-cutted=$(echo "$cutted" | sed -e '9,10d' | sed -e '1,6{H;d}; ${p;x;s/^\n//}')
+#cutted=$(echo "$cutted" | sed -e '9,10d' | sed -e '1,6{H;d}; ${p;x;s/^\n//}')
 
 echo "$cutted"
-
+fi
